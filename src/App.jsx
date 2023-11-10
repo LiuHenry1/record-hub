2a2aa45ceabea41e0b41208ebdb39736850268fc
Henry Liu <hliu4176@gmail.com>
2023-11-10 09:55:32 -0600

    feat: Layout HTML structure
    Create header to link to other pages. Layout where the main content will
    display.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import "./App.css";
+import { useRoutes, Link } from "react-router-dom";
+import { IoIosFitness } from "react-icons/io";
+import Feed from "./pages/Feed";
 
 function App() {
-  
+  let main = useRoutes([{ path: "/", element: <Feed /> }]);
 
   return (
     <>
-      
+      <div className="header">
+        <div className="header-logo">
+          <IoIosFitness className="icon" />
+          <h4>Record Hub</h4>
+        </div>
+        <div className="header-search">
+          <input type="text" placeholder="Search..." />
+        </div>
+        <nav className="header-nav">
+          <Link to="/">Home</Link>
+          <Link to="/new">Create Post</Link>
+        </nav>
+      </div>
+      <div className="main">{main}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
