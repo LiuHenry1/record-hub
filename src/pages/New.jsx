61cc8f544cedd1d565098cb1ec20890f8045f613
Henry Liu <hliu4176@gmail.com>
2023-11-12 17:03:27 -0600

    feat: receive state setter to refetch data after posting
    Instead of refreshing page after posting data, use states to rerender.

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,36 +1,52 @@
-import "./New.css"
-import { supabase } from "../client"
+import "./New.css";
+import { supabase } from "../client";
 import { useState } from "react";
 
-const New = () => {
+const New = ({ onUpdate }) => {
   const [post, setPost] = useState({
     title: "",
-    content: ""
+    content: "",
   });
 
   const handleChange = (e) => {
     const field = e.target.name;
     const value = e.target.value;
 
-    setPost({...post, [field]: value});
-  }
+    setPost({ ...post, [field]: value });
+  };
 
-  const handleClick = async(e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
 
     await supabase.from("Posts").insert(post).select();
 
-    window.location = "/new"
-  }
+    setPost({ title: "", content: "" });
+    onUpdate((toggle) => !toggle);
+  };
 
   return (
-  <div className="form-container">
-    <form className="new-post"> 
-      <input onChange={handleChange} type="text" name="title" placeholder="Title" />
-      <textarea onChange={handleChange} type="text" name="content" placeholder="Content" rows={8} cols={32}/>
-      <input onClick={handleClick} type="button" value="Create Post" />
-    </form>
-  </div>);
+    <div className="form-container">
+      <form className="new-post">
+        <input
+          onChange={handleChange}
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={post.title}
+        />
+        <textarea
+          onChange={handleChange}
+          type="text"
+          name="content"
+          placeholder="Content"
+          rows={8}
+          cols={32}
+          value={post.content}
+        />
+        <input onClick={handleClick} type="button" value="Create Post" />
+      </form>
+    </div>
+  );
 };
 
 export default New;
