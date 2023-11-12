df576a223c5fc57a629a0d23b0c508fec01265ba
Henry Liu <hliu4176@gmail.com>
2023-11-12 17:28:48 -0600

    feat: add functionality to update content of post

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,20 +1,68 @@
 import { useParams } from "react-router-dom";
-import "./Edit.css"
+import "./Edit.css";
+import { useEffect, useState } from "react";
+import { supabase } from "../client";
 
-const Edit = ({posts}) => {
+const Edit = ({ data, onUpdate }) => {
+  const [post, setPost] = useState(null);
   const { id } = useParams();
-  const post = posts.filter((post) => post.id == id)[0];
+
+  useEffect(() => {
+    if (data) {
+      setPost(data.filter((post) => post.id == id)[0]);
+    }
+  }, [data]);
+
+  const handleChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+
+    setPost({ ...post, [field]: value });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    await supabase
+      .from("Posts")
+      .update({
+        title: post.title,
+        content: post.content,
+      })
+      .eq("id", id)
+      .select();
+
+    alert("Updated!");
+    onUpdate((toggle) => !toggle);
+  };
 
   return (
-  <div className="form-container">
-    <form className="update-post"> 
-      <label for="title">Title</label>
-      <input type="text" name="title" value={post.title}/>
-      <label for="content">Content</label>
-      <textarea type="text" name="content" value={post.content} rows={8} cols={32}/>
-      <input type="button" value="Update Post" />
-    </form>
-  </div>);
+    <>
+      {post && (
+        <div className="form-container">
+          <form className="update-post">
+            <label for="title">Title</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="title"
+              value={post.title}
+            />
+            <label for="content">Content</label>
+            <textarea
+              onChange={handleChange}
+              type="text"
+              name="content"
+              value={post.content}
+              rows={8}
+              cols={32}
+            />
+            <input onClick={handleClick} type="button" value="Update Post" />
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Edit;
