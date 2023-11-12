103067fae1c131a10812e09c8ce7b35df53494e3
Henry Liu <hliu4176@gmail.com>
2023-11-11 21:29:17 -0600

    feat: add functionality to create entry in database

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,11 +1,34 @@
 import "./New.css"
+import { supabase } from "../client"
+import { useState } from "react";
+
 const New = () => {
+  const [post, setPost] = useState({
+    title: "",
+    content: ""
+  });
+
+  const handleChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+
+    setPost({...post, [field]: value});
+  }
+
+  const handleClick = async(e) => {
+    e.preventDefault();
+
+    await supabase.from("Posts").insert(post).select();
+
+    window.location = "/new"
+  }
+
   return (
   <div className="form-container">
     <form className="new-post"> 
-      <input type="text" name="title" placeholder="Title" />
-      <textarea type="text" name="content" placeholder="Content" rows={8} cols={32}/>
-      <input type="button" value="Create Post" />
+      <input onChange={handleChange} type="text" name="title" placeholder="Title" />
+      <textarea onChange={handleChange} type="text" name="content" placeholder="Content" rows={8} cols={32}/>
+      <input onClick={handleClick} type="button" value="Create Post" />
     </form>
   </div>);
 };
