3c81e57592143328ca77f8a86020fb75842eb575
Henry Liu <hliu4176@gmail.com>
2023-11-14 14:55:06 -0600

    feat: add filter by search

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import { IoIosFitness } from "react-icons/io";
 import Feed from "./pages/Feed";
 import New from "./pages/New";
 import Post from "./pages/Post";
-import Posts from "./data.json"
 import Edit from "./pages/Edit";
 import { useEffect, useState } from "react";
 import { supabase } from "./client";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [postsToDisplay, setPostsToDisplay] = useState([]);
   const [toggle, setToggle] = useState(false);
   const [sort, setSort] = useState({
     orderBy: "created_at",
-    condition: { ascending: true }
-  })
+    condition: { ascending: true },
+  });
+  const [search, setSearch] = useState("");
 
+  const handleChange = (e) => {
+    const toSearch = e.target.value.toLowerCase();
+
+    setSearch(toSearch);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -25,17 +31,32 @@ function App() {
         .select()
         .order(sort.orderBy, sort.condition);
 
-        setPosts(data);
+      setPosts(data);
     };
 
     fetchPosts();
-  }, [toggle, sort])
+  }, [toggle, sort]);
+
+  const filterPosts = () => {
+    if (search == "") {
+      setPostsToDisplay(posts);
+    } else {
+      const filteredPosts = posts.filter((post) =>
+        post.title.toLowerCase().includes(search)
+      );
+      setPostsToDisplay(filteredPosts);
+    }
+  }
+
+  useEffect(() => {
+    filterPosts();
+  }, [search, posts]);
 
   let main = useRoutes([
-    { path: "/", element: <Feed data={posts} updateSort={setSort}/> },
-    { path: "/new", element: <New onUpdate={setToggle}/>},
-    { path: "/post/:id", element: <Post data={posts} onUpdate={setToggle}/>},
-    { path: "/edit/:id", element: <Edit data={posts} onUpdate={setToggle}/>},
+    { path: "/", element: <Feed data={postsToDisplay} updateSort={setSort} /> },
+    { path: "/new", element: <New onUpdate={setToggle} /> },
+    { path: "/post/:id", element: <Post data={posts} onUpdate={setToggle} /> },
+    { path: "/edit/:id", element: <Edit data={posts} onUpdate={setToggle} /> },
   ]);
 
   return (
@@ -46,7 +67,7 @@ function App() {
           <h4>Record Hub</h4>
         </div>
         <div className="header-search">
-          <input type="text" placeholder="Search..." />
+          <input onChange={handleChange} type="text" placeholder="Search..." />
         </div>
         <nav className="header-nav">
           <Link to="/">Home</Link>
